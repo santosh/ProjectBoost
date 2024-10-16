aecec0c40c3749543362f75fe3c4a8ed0b2f7851
Santosh Kumar <sntshkmr60@gmail.com>
2024-10-16 17:52:34 +0530

    add physics to player & add kbd and controller support

--- player.gd
+++ player.gd
@@ -1,17 +1,18 @@
-extends Node3D
+extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("player entered the scene")
+	print("you are now on launch pad")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * 1000)
 		
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, 100.0 * -delta))
+
