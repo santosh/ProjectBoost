b53179be1e5035839383200733f3373c68f4f6d8
Santosh Kumar <sntshkmr60@gmail.com>
2024-10-16 17:53:45 +0530

    add winning contition

--- player.gd
+++ player.gd
@@ -16,3 +16,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, 100.0 * -delta))
 
+
+func _on_body_entered(body: Node) -> void:
+	if "Hazard" in body.get_groups():
+		print("you crashed!")
+	elif "Goal" in body.get_groups():
+		print("you won! you landed on landing pad")
+	elif "Start" in body.get_groups():
+		print("you are back on launch pad")
