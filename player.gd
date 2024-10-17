a658153f09aade78457bde94da910c3aa6d99cbc
Santosh Kumar <sntshkmr60@gmail.com>
2024-10-17 16:47:16 +0530

    refactor magic numbers, export vars to editor

--- player.gd
+++ player.gd
@@ -1,22 +1,27 @@
 extends RigidBody3D
 
+## Amount of vertical force to apply on rocket.
+@export_range(800, 1200) var thurst: float = 1000.0
+
+## Amount of axial boost when turning.
+@export var torque_thrust: float = 100.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("you are now on launch pad")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000)
+		apply_central_force(basis.y * delta * thurst)
 		
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * -delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
-
+# Called on frame on which rigid body comes in contact with other bodies.
 func _on_body_entered(body: Node) -> void:
 	if "Hazard" in body.get_groups():
 		print("you crashed!")
